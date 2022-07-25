76e81bad210850986b2b09d401191353e3ebc502
unknown <sapnakumawat303@gmail.com>
2022-07-25 11:53:35 +0530

    Migration create for product and rewvew

--- routes/api.php
+++ routes/api.php
@@ -22,5 +22,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::apiResource('/products','App\Http\Controllers\ProductController');
 
 Route::group(['prefix'=>'products'],function(){
-    Route::apiResource('/{product}reviews','App\Http\Controllers\ReviewsController');
+    Route::apiResource('/{product}/reviews','App\Http\Controllers\ReviewsController');
 });
